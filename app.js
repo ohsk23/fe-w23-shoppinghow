d588511e0c027675a39e7771d9dac399f508a83c
nina.oh <nina.oh@kakaocorp.com>
2021-01-11 22:23:39 +0900

    Make header and navbar structure

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 app.engine('html', require('ejs').renderFile)
-
 
 app.get('/', (req, res) => {
     res.render('./index.html')
